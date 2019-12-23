28dc971ff7f64e996896ddd42160c5099652dfa3
bcn0024 <bcn0024@icloud.com>
2019-12-23 19:37:41 +0900

    メッセージ一覧にアイコン追加
    上記とuserのshowに直打ちして飛べないようにせき止めた。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,19 @@
 class UsersController < ApplicationController
-  
+  before_action :set_user, only:[:move_to_index, :show]
+  before_action :move_to_index, only:[:show]
+
   def index
   end
   
   def show
-    @user = User.find(params[:id])
     @groups = @user.groups
   end
+  
+  def move_to_index
+    redirect_to root_path unless user_signed_in? && current_user.id == @user.id
+  end
 
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
