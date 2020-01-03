04e6a42c135eef7eb827bf469265aee5b0b0a178
bcn0024 <bcn0024@icloud.com>
2020-01-03 14:50:54 +0900

    nginx
    導入

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,4 +1,4 @@
-#サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく
+
 app_path = File.expand_path('../../', __FILE__)
 
 worker_processes 1
@@ -7,18 +7,17 @@ working_directory app_path
 
 pid "#{app_path}/tmp/pids/unicorn.pid"
 
-listen 3000
+listen "#{app_path}/tmp/sockets/unicorn.sock"
 
-#エラーのログを記録するファイルを指定
 stderr_path "#{app_path}/log/unicorn.stderr.log"
 
-#通常のログを記録するファイルを指定
+
 stdout_path "#{app_path}/log/unicorn.stdout.log"
 
-#Railsアプリケーションの応答を待つ上限時間を設定
+
 timeout 60
 
-#以下は応用的な設定なので説明は割愛
+
 
 preload_app true
 GC.respond_to?(:copy_on_write_friendly=) && GC.copy_on_write_friendly = true
