e6a50f0de3b954668a724d23e6456f1e4807b10a
bcn0024 <bcn0024@icloud.com>
2020-01-14 19:40:53 +0900

    ユーザー登録のテストコードを記載

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 describe User do
   describe '#create' do
     it "is valid with a nickname, email, password, password_confirmation" do
-      user = build(:user)
+      # user = build(:user)
+      user = User.new(name: "abe", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
       expect(user).to be_valid
     end
   end
